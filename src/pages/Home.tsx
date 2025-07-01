2870374b5e1a0e22d3ebd81d683457e7d02b9e1b
Roberto Andino <andinorobert518@yahoo.com>
2025-07-01 13:47:43 -0400

    Add user challenge information to profile dropdown

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -65,7 +65,7 @@ const Home: React.FC = () => {
     }, [])
 
     console.log(setProfileOpen);
-    
+
     return (
         <main className="main-container">
             {/*Main Header*/}
@@ -82,6 +82,9 @@ const Home: React.FC = () => {
                     {profileOpen && (
                         <div className="profile-dropdown">
                             <h4>User Info</h4>
+                            <div><strong>Daily Challenge:</strong> {dailyChallenge} </div>
+                            <div><strong>Challenges Completed:</strong> {completedCount}</div>
+                            <div><strong>Current Challenge:</strong> {currentChallenge}</div>
                         </div>
                     )}
                 </div>
