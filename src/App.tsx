793ebbd1cba93717958a13a7a1b99f91c44fbef3
robertoandino <andinorobert518@yahoo.com>
2025-12-31 11:34:53 -0500

    Add React router routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.tsx'
+import Profile from './components/Profile/Profile'
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} /> 
+      </Routes>
+    </Router>
   )
 }
 
